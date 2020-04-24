ffa725b0f3245026b283fd695bbafbfac13c6b6f
Kris <kristhecanadian101@gmail.com>
2020-04-24 14:13:13 -0400

    refactor

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+require('dotenv/config');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//Middlewares
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//Import Routes
+const starwarsRoute = require('./routes/starwars')
+
+app.use('/starwars', starwarsRoute);
+
+//Routes
+app.get('/', (req, res) => {
+
+  res.send("We are on home");
+
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//Connect To DB
+// useUnifiedTopology: true creates a timout, removed for now until I find a fix
+const uri = process.env.DB_CONNECTION;
+const client = new MongoClient(uri, {useNewUrlParser: true });
+client.connect(err => {
+  console.log('Connected to DB!');
+  // perform actions on the collection object
+  client.close();
 });
-
-module.exports = app;
+//Listening
+app.listen(3000);
